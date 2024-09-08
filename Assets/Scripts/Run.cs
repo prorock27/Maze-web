7d3f7c617657fa12fb86863135abdcb4b6d57aa8
TheNija172 <vadim.bagay@mail.ru>
2024-09-08 18:21:59 +0300

    0.0.0.1

---
+++ Assets/Scripts/Run.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+
+public class Run : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+    public Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * moveSpeed;
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            animator.SetBool("isRun", true);
+        }
+        else
+        {
+            animator.SetBool("isRun", false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
